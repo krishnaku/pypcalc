c6b5af258db818d08271326c620fc193ceba42be
Krishna Kumar <kkumar@exathink.com>
2025-04-26 11:31:58 -0500

    Refactored to prepare for multi-run simulations.

--- sim/runtime/request_response/request_response_sim.py
+++ sim/runtime/request_response/request_response_sim.py
@@ -14,23 +14,23 @@ import random
 import threading
 from collections import deque
 import time
-from typing import Callable
+from typing import Callable, List, Tuple
 
+import simpy
 from IPython.core.display_functions import display
 from matplotlib import pyplot as plt, animation
-
 
 from sim.runtime.request_response.collaborator import Collaborator, Request, Response
 from sim.runtime.simulation import Simulation
 
 log = logging.getLogger(__name__)
 
+
 class Requestor(Collaborator):
-    def __init__(self, name:str, sim_context: Simulation,  mean_time_between_requests=2):
-        super().__init__(name, sim_context,  processing_time=0, capacity=None)
+    def __init__(self, name: str, sim_context: Simulation, mean_time_between_requests=2):
+        super().__init__(name, sim_context, processing_time=0, capacity=None)
         self.counter = 0
         self.mean_time_between_requests = mean_time_between_requests
-
 
     def run(self):
         while True:
@@ -40,7 +40,6 @@ class Requestor(Collaborator):
             )
             self.counter += 1
             self.send(request)
-
 
             delay = random.expovariate(1 / self.mean_time_between_requests)
             yield self.env.timeout(delay)
@@ -55,27 +54,44 @@ class Responder(Collaborator):
 
 
 class RequestResponseSimulation(Simulation):
-    def __init__(self, requestor: Callable[[Simulation], Requestor], responder: Callable[[Simulation], Responder], until=20, metrics_poll_interval=0.2, realtime_factor=None):
-        super().__init__(realtime_factor)
-        self.queue_samples = deque()
+    def __init__(
+            self,
+            requestor: Callable[[Simulation], Requestor],
+            responder: Callable[[Simulation], Responder],
+            runs=1,
+            until=20,
+            metrics_poll_interval=0.2,
+            realtime_factor=None):
+        self.queue_samples: List[List[Tuple[float, int]]] = [[] for run in range(runs)]
         self.sample_lock = threading.Lock()
         self.metrics_poll_interval = metrics_poll_interval
-        self.until = until
+        self.requestor_factory = requestor
+        self.responder_factory = responder
+        self.requestor = None
+        self.responder = None
 
-        self.requestor = requestor(self)
-        self.responder = responder(self)
+        # init the simulation parameters and model
+        super().__init__(
+            until,
+            runs,
+            realtime_factor
+        )
 
-
+    def bind_environment(self, env: simpy.Environment):
+        # this is called every time the simulation environment is reset
+        self.requestor = self.requestor_factory(self)
+        self.responder = self.responder_factory(self)
         self.requestor.set_peer(self.responder)
 
-
-
+        env.process(self.requestor.run())
+        env.process(self.requestor.receive())
+        env.process(self.responder.receive())
+        env.process(self.queue_monitor(self.metrics_poll_interval))
 
     def queue_monitor(self, interval=0.1):
-        """Push (sim_time, queue_len) into queue_samples every *interval*."""
         while True:
             with self.sample_lock:
-                self.queue_samples.append((self.env.now, self.responder.entity_count))
+                self.queue_samples[self.current_run].append((self.env.now, self.responder.entity_count))
             yield self.env.timeout(interval)
 
     def plot(self, interval=30, arrival_rate=None, service_rate=None):
@@ -88,21 +104,25 @@ class RequestResponseSimulation(Simulation):
         ax.set_ylabel("queue length")
         ax.legend()
 
-        utilization = arrival_rate/service_rate
-        target_average = (utilization*utilization)/(1-utilization)
+        target_average = None
+        if arrival_rate is not None and service_rate is not None:
+            utilization = arrival_rate / service_rate
+            if utilization < 1:
+                # if utilization > 1 the system is unstable and there is no target average.
+                target_average = (utilization * utilization) / (1 - utilization)
 
         def init():
             ax.set_xlim(0, self.until)
-            ax.set_ylim(0, target_average*2)
+            ax.set_ylim(0, target_average * 2)
             return ax_line, avg_line, target_line
 
         def update(frame):
             with self.sample_lock:
-                if not self.queue_samples:
+                if not self.queue_samples[self.current_run]:
                     print(f"Nothing to plot")
-                    return ax_line,avg_line,target_line
+                    return ax_line, avg_line, target_line
 
-                xs, ys = zip(*self.queue_samples)
+                xs, ys = zip(*(self.queue_samples[self.current_run]))
 
             # main queue length line
             ax_line.set_data(xs, ys)
@@ -111,7 +131,8 @@ class RequestResponseSimulation(Simulation):
             avg_line.set_data(xs, cumulative_avg)
 
             # Theoretical average line
-            target_line.set_data([xs[0], xs[-1]], [target_average, target_average])
+            if target_average is not None:
+                target_line.set_data([xs[0], xs[-1]], [target_average, target_average])
 
             ax.set_ylim(0, max(5, max(list(ys) + cumulative_avg + [target_average]) + 1))
             return ax_line, avg_line, target_line
@@ -120,21 +141,9 @@ class RequestResponseSimulation(Simulation):
                                       interval=interval, blit=False, cache_frame_data=False)
         fig._ani = ani
 
-
         return ani
 
-    def init_processes(self):
-        self.env.process(self.requestor.run())
-        self.env.process(self.requestor.receive())
-        self.env.process(self.responder.receive())
-        self.env.process(self.queue_monitor(self.metrics_poll_interval))
 
-    def run(self):
-        print(f"Simulation started at {self.env.now}")
-        self.init_processes()
-        self.env.run(until=self.until)
-        print(f"simulation ended at {self.env.now}")
-        print(str(self.signal_log))
 
 
 
@@ -147,12 +156,12 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     sim = RequestResponseSimulation(
         requestor=lambda sim: Requestor(name="A", sim_context=sim, mean_time_between_requests=2),
-        responder =  lambda sim: Responder(name="B", sim_context=sim, processing_time=1.5, capacity=1),
+        responder=lambda sim: Responder(name="B", sim_context=sim, processing_time=1.5, capacity=1),
         until=3000,
         realtime_factor=None
     )
+    #sim.run()
 
-    ani = sim.plot(interval=10, arrival_rate=0.5, service_rate=1/1.5)
+    ani = sim.plot(interval=10, arrival_rate=0.5, service_rate=1 / 1.5)
     threading.Thread(target=sim.run, daemon=True).start()
     plt.show()
-
--- sim/runtime/simulation.py
+++ sim/runtime/simulation.py
@@ -10,10 +10,52 @@ from core.signal import SignalLog
 # Author: Krishna Kumar
 import simpy
 
+from typing import List
 class Simulation:
-    def __init__(self, realtime_factor: float = None):
-        self._env = simpy.rt.RealtimeEnvironment(factor=realtime_factor) if realtime_factor is not None else simpy.Environment()
+    def __init__(self, until=30, runs=1, realtime_factor: float = None):
+        self._env = None
+        self._signal_log = None
+        # preserve separate signal logs per simulation run
+        self._all_logs: List[SignalLog] = []
+
+        self.realtime_factor = realtime_factor
+        self.until = until
+        self.runs = runs
+        self.current_run = 0
+
+
+
+    def init_sim(self):
+        if self.realtime_factor is not None:
+            self._env = simpy.rt.RealtimeEnvironment(factor=self.realtime_factor)
+        else:
+            self._env = simpy.Environment()
+
+        self.reset_signal_log()
+
+        self.bind_environment(self._env)
+
+    def reset_signal_log(self):
+        if self._signal_log is not None:
+            self._all_logs.append(self._signal_log)
+
         self._signal_log = SignalLog()
+
+    def bind_environment(self, env):
+        pass
+
+    def run(self):
+        print(f"Simulation started")
+        for self.current_run in range(self.runs):
+            self.init_sim()
+            self.env.run(until=self.until)
+            self.post_run()
+
+        print(f"simulation ended")
+        print(str(self.signal_log))
+
+    def post_run(self):
+        pass
 
     @property
     def env(self):
@@ -23,4 +65,6 @@ class Simulation:
     def signal_log(self):
         return self._signal_log
 
-
+    @property
+    def all_logs(self) -> List[SignalLog]:
+        return self._all_logs + ([self._signal_log] if self._signal_log else [])
